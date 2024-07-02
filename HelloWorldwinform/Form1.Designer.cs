89ab6af05fb2327654c57f9e8e51a03a21efca3c
github kang <etock19@naver.com>
2024-07-02 18:27:47 +0900

    사진 추가/변경

--- HelloWorldwinform/Form1.Designer.cs
+++ HelloWorldwinform/Form1.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -46,7 +45,7 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Image = global::HelloWorldwinform.Properties.Resources.computer_sagyouin_woman;
             this.pictureBox1.Location = new System.Drawing.Point(12, 157);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(804, 486);
